8bcff85c1d94ae6308ed0d838532efb675d9247b
Conor Smith <csmith55@qub.ac.uk>
2017-12-30 23:05:49 +0000

    Adding in support for TTV

--- firetv/__init__.py
+++ firetv/__init__.py
@@ -523,6 +523,7 @@ class FireTV:
         self.enter()
         time.sleep(5)
         adb_show = show.replace(" ","%")
+        logging.error("AHHHHHHH '%s'", adb_show)
         self._adb.Shell("input text" + adb_show)
         self.media_play_pause()
         time.sleep(2)
